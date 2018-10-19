0db96d2c1265349bf8535756507e15edb1852f0f
Pablo Costanzo <costanzopa@gmail.com>
2018-10-19 01:01:58 -0300

    Fix edit image and google maps.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,10 @@
 var loadFile = function(event) {
-    var reader = new FileReader();
-    reader.onload = function(){
-        var output = document.getElementById('image');
-        output.src = reader.result;
+    var reader  = new FileReader();
 
+    reader.onload = function(){
+        var output = document.getElementById('image-preview');
+        output.src = reader.result;
+        output.value = event.target.files[0].name;
     };
     reader.readAsDataURL(event.target.files[0]);
     document.getElementById("image-name").value = event.target.files[0].name;
