dc97530ea531c7a6f3d1624769aed2b8df011c18
Pablo Costanzo <costanzopa@gmail.com>
2019-05-31 23:07:31 -0300

    Make prettier js.

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,15 +1,14 @@
 var mongoose = require("mongoose");
 
 var commentSchema = new mongoose.Schema({
-    text: String,
-    createdAt: { type: Date, default: Date.now },
-    author: {
-        id:  {
-            type: mongoose.Schema.Types.ObjectId
-        },
-        username: String
-    }
+  text: String,
+  createdAt: { type: Date, default: Date.now },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId
+    },
+    username: String
+  }
 });
-
 
 module.exports = mongoose.model("Comment", commentSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,52 +1,55 @@
-var express  = require("express"),
-    router   = express.Router(),
-    passport = require("passport/lib"),
-    User     = require("../models/user");
+var express = require("express"),
+  router = express.Router(),
+  passport = require("passport/lib"),
+  User = require("../models/user");
 
 // "/" Landing Page
-router.get("/", function (req, res) {
-    res.render("landing");
+router.get("/", function(req, res) {
+  res.render("landing");
 });
 
 // show register form
-router.get("/register", function(req, res){
-    res.render("register", {page: 'register'});
+router.get("/register", function(req, res) {
+  res.render("register", { page: "register" });
 });
 
 // Create a user
-router.post("/register", function (req, res) {
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function (err, user) {
-        if(err) {
-            console.log(err);
-            return res.render("register", {error: err.message});
-        }
-        passport.authenticate("local")(req, res, function () {
-            req.flash("success",  "Successfully Signed Up! Nice to meet you " + user.username);
-            res.redirect("/holygrounds")
-        });
+router.post("/register", function(req, res) {
+  var newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      return res.render("register", { error: err.message });
+    }
+    passport.authenticate("local")(req, res, function() {
+      req.flash(
+        "success",
+        "Successfully Signed Up! Nice to meet you " + user.username
+      );
+      res.redirect("/holygrounds");
     });
+  });
 });
 
-
 //show login form
-router.get("/login", function(req, res){
-    res.render("login", {page: 'login', message: req.flash("error")});
+router.get("/login", function(req, res) {
+  res.render("login", { page: "login", message: req.flash("error") });
 });
 
 // handling login logic
-router.post("/login", passport.authenticate("local",
-    {
-        successRedirect: "/holygrounds",
-        failureRedirect: "/login"
-    }), function(req, res){
-});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/holygrounds",
+    failureRedirect: "/login"
+  }),
+  function(req, res) {}
+);
 
-
-router.get("/logout", function (req, res) {
-    req.logout();
-    req.flash("success","Logged you out!");
-    res.redirect("/holygrounds");
+router.get("/logout", function(req, res) {
+  req.logout();
+  req.flash("success", "Logged you out!");
+  res.redirect("/holygrounds");
 });
 
 module.exports = router;
